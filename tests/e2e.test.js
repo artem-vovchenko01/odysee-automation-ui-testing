59a0a2a1d9bf5094a67417cc53182ca59fd22e89
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-12-17 12:37:30 +0200

    Initial commit

---
+++ tests/e2e.test.js
@@ -0,0 +1,97 @@
+const odysee = require('../utilities/e2eOdysee')
+const timeout = 8000000
+const { userName, password } = require('../utilities/cred')
+const domain = 'https://odysee.com/$/signin'
+
+describe(
+    'E2E test as an end-user, perform the tests below:',
+    () => {
+        let loginPage;
+        let mainPage;
+        let searchResultPage;
+
+        beforeAll(
+            async () => {
+                try {
+                    loginPage = await odysee.startTest(domain, false)
+                    // mainPage = await loginPage.login(userName, password)
+                }
+                catch (error) {
+                    console.log(error);
+                    throw error;
+                }
+            }, timeout
+        );
+        // it("Page shows error when incorrect login data is entered",
+        //     async () => {
+        //         console.log("TEST 1")
+        //         await reporter.description("Test invalid logi attempt")
+        //         await reporter.startStep("Enter wrong phone number and password")
+        //         await loginPage.loginForValidation(userName, '1234')
+        //         await reporter.endStep()
+
+        //         await reporter.startStep("Check if page shows an error")
+        //         const loginError = await loginPage.getLoginError()
+        //         await reporter.endStep()
+                
+        //         expect(loginError).toBeDefined()
+        //         expect(loginError).toBe("Incorrect email and/or password")
+        //         await loginPage.goToLogin(domain)
+
+        //     }, timeout
+        // )
+
+        it("Login succeeds with correct credentials",
+            async () => {
+                console.log("TEST 2")
+                await reporter.description("Test valid login attempt")
+                await reporter.startStep("Enter correct credentials")
+                mainPage = await loginPage.loginForFurtherTesting(userName, password)
+                await reporter.endStep()
+
+                await reporter.startStep("Check if page shows an error")
+                const loginError = await loginPage.getLoginError()
+                await reporter.endStep()
+                
+                expect(loginError).toBe(null)
+            }, timeout
+        )
+
+        it("Logout works", 
+            async () => {
+                console.log("TEST 3")
+                expect(await mainPage.logout()).toBe(true)
+                await loginPage.goToLogin(domain)
+            }, timeout
+        )
+
+        it('Search works',
+            async () => {
+                console.log("TEST 5")
+                await loginPage.loginForFurtherTesting(userName, password)
+                expect(await mainPage.search('linux')).toBe(true)
+                expect(await mainPage.search('Luke Smith')).toBe(true)
+                await loginPage.wait(2000)
+            }, timeout
+        )
+
+        it('Like works',
+            async () => {
+                const title = 'the linux experiment'
+                await mainPage.search(title)
+                await mainPage.goToSearchResult(0)
+                if (await mainPage.isVideoLiked()) { await mainPage.unlikeVideo() }
+                expect(await mainPage.isVideoLiked()).toBe(false)
+                expect(await mainPage.likeVideo()).toBe(true)
+                expect(await mainPage.isVideoLiked()).toBe(true)
+            }, timeout
+        ),
+
+        afterAll(
+            async () => {
+                await e2eGitHub.endTest()
+            }
+        );
+    }, timeout
+);
+
---
+++ utilities/e2eOdysee.js
@@ -0,0 +1,112 @@
+const puppeteer = require("puppeteer");
+const { userName, password } = require("./cred");
+
+
+class e2eOdysee {
+    async startTest(domain, headlessStatus) {
+        this.browser = await puppeteer.launch({ headless: headlessStatus });
+        let page = await this.browser.newPage();
+        page.setDefaultTimeout(1000 * 60 * 5);
+        await page.setViewport({ width: 1920, height: 1080 })
+        await page.goto(domain, { waitUntil: 'networkidle0' });
+        return this.loginPage(page)
+    }
+
+    async loginPage(odyseePage) {
+        async function login(userName, password) {
+            await odyseePage.waitForSelector('input[id=username]', { visible: true });
+            await odyseePage.type('input[id=username]', userName, { delay: 50 });
+            await odyseePage.click('button.button--primary')
+            await odyseePage.waitForSelector('input[id=password]');
+            await odyseePage.type('input[id=password]', password, { delay: 50 });
+            await odyseePage.waitForSelector('button.button--primary:not([disabled])', {visible: true})
+            await odyseePage.click('button.button--primary');
+            await odyseePage.waitFor(4000)
+            // return this.mainPage(odyseePage);
+        }
+        return {
+            goToLogin: async(domain) => {
+                await odyseePage.goto(domain)
+            },
+
+            wait: async(msec) => {
+                await odyseePage.waitFor(msec)
+            },
+
+            loginForValidation: async (userName, password) => {
+                await login(userName, password)
+            },
+
+            loginForFurtherTesting: async(userName, password) => {
+                await login(userName, password)
+                return this.mainPage(odyseePage)
+            },
+
+            getLoginError: async () => {
+                try {
+                    await odyseePage.waitForSelector('.nag--error > .nag__message', {timeout: 3000})
+                } catch(e) { return null }
+
+                let err = await odyseePage.$eval('.nag--error > .nag__message', el => el.textContent)
+                return err
+                }
+            }
+        }
+    async mainPage(odyseePage) {
+        return {
+            logout: async () => {
+                await odyseePage.waitForSelector('button[aria-label="Your account"', {visible: true})
+                await odyseePage.waitFor(2000)
+                await odyseePage.click('button[aria-label="Your account"')
+                await odyseePage.waitForSelector('div[role="menuitem"]', {visible: true})
+                await odyseePage.waitFor(2000)
+                await odyseePage.click('div[role="menuitem"]')
+                try {
+                    await odyseePage.waitForSelector('a.button--link[aria-label="Log In"]', {timeout: 3000})
+                } catch(e) {return false}
+                return true
+            },
+            search: async (searchTerm) => {
+                await odyseePage.waitForSelector('input[placeholder=Search]');
+                await odyseePage.click('input.wunderbar__input');
+                await odyseePage.type('input.wunderbar__input', searchTerm, { delay: 100 });
+                await odyseePage.keyboard.press('Enter');
+                await odyseePage.waitForSelector('.claim-preview__wrapper', {timeout: 10000})
+                await odyseePage.waitFor(7000)
+                await odyseePage.click('input.wunderbar__input');
+                const inputValue = await odyseePage.$eval('input.wunderbar__input', el => el.value);
+                for (let i = 0; i < inputValue.length; i++) {
+                    await odyseePage.keyboard.press('Backspace');
+                }
+                return await odyseePage.evaluate((searchTerm) => {
+                    return [...document.querySelectorAll('.claim-preview__title > span')].map(s => s.textContent).filter(str => !str.toLowerCase().includes(searchTerm.toLowerCase())).length == 0
+                }, searchTerm)
+            },
+            goToSearchResult: async(index) => {
+                let elems = await odyseePage.$$('.claim-preview__wrapper:not(.claim-preview__wrapper--channel)')
+                await elems[index].click()
+                await odyseePage.waitForSelector('.media__actions', {visible: true})
+            },
+            isVideoLiked: async() => {
+                return await odyseePage.evaluate(() => document.querySelector('.icon--FireActive') != null)
+            },
+            likeVideo: async() => {
+                await odyseePage.click('button[title="I like this"]')
+                try {
+                    await odyseePage.waitForSelector('.icon--FireActive', {timeout: 5000})
+                } catch(e) {return false}
+                return true
+            },
+            unlikeVideo: async() => {
+                await odyseePage.click('button[title="I like this"]')
+                await odyseePage.waitFor(3000)
+            },
+        }
+    }
+
+    async endTest() {
+        await this.browser.close();
+    }
+
+}
+module.exports = new e2eOdysee()
